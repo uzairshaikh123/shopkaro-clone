7f648c2886bc3b69421981ad09caecd7311b28b8
Vicky Kumar <110033953+vicky-masai@users.noreply.github.com>
2023-02-22 19:21:01 +0530

    db.json file added more keys

--- shopkaro/shopkaro/src/dashboard/components/Side.jsx
+++ shopkaro/shopkaro/src/dashboard/components/Side.jsx
@@ -4,7 +4,6 @@ import styles from '@/styles/Home.module.css'
 const Side = () => {
   return (
     <div className={styles.nav}><img src="https://vicky-masai.github.io/images/vicky-profile-trans.png" alt="Vicky Profile Pic"/>
-        <Link href={"/"}>Home</Link>
         <Link href={"/dashboard"}>Dashboard</Link>
         <Link href={"/orders"}>Orders</Link>
         <Link href={"/addproduct"}>Add Product</Link>
