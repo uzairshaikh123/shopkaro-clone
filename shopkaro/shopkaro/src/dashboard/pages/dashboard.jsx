8c0a46e1cdc2478a512753b4a3b11e9e56de85a7
Vicky Kumar <110033953+vicky-masai@users.noreply.github.com>
2023-02-22 15:47:55 +0530

    Menu ui + dashboard ui ready with valina css

---
+++ shopkaro/shopkaro/src/dashboard/pages/dashboard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Side from '@/components/Side'
+import styles from '@/styles/Home.module.css'
+const Dashboard = () => {
+    return (
+        <div className={styles.main}>
+    <Side />
+    <div className={styles.cont}>
+    <h1>Dashboard</h1>
+    </div>
+
+    <div className={styles.grd}>
+        <div><div style={{backgroundColor:"rgba(102, 0, 255, 0.78)"}}>10</div><h3>Total Orders</h3></div>
+        <div><div style={{backgroundColor:"rgba(102, 0, 255, 0.78)"}}>10</div><h3>Total Earning</h3></div>
+        <div><div style={{backgroundColor:"rgba(102, 0, 255, 0.78)"}}>10</div><h3>Total Products</h3></div>
+        <div><div style={{backgroundColor:"rgba(102, 0, 255, 0.78)"}}>10</div><h3>Total Users</h3></div>
+    </div>
+        </div>
+      )
+    }
+
+export default Dashboard
---
+++ shopkaro/shopkaro/src/dashboard/pages/orders.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Side from '@/components/Side'
+import styles from '@/styles/Home.module.css'
+const Orderlist = () => {
+  return (
+    <div className={styles.main}>
+<Side />
+<div className={styles.cont}>
+<h1>orders</h1>
+</div>
+    </div>
+  )
+}
+
+export default Orderlist
