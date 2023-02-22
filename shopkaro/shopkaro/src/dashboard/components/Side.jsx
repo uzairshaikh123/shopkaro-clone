7d5f82cb1110389e645700daccf5db46c03e045f
Vicky Kumar <110033953+vicky-masai@users.noreply.github.com>
2023-02-22 20:20:36 +0530

    Dashboard ui+full functionality done

--- shopkaro/shopkaro/src/dashboard/components/Side.jsx
+++ shopkaro/shopkaro/src/dashboard/components/Side.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
 const Side = () => {
   return (
-    <div className={styles.nav}><img src="https://vicky-masai.github.io/images/vicky-profile-trans.png" alt="Vicky Profile Pic"/>
+    <div className={styles.nav}><img src="https://vicky-masai.github.io/images/vicky-profile-trans.png" alt="Vicky Profile Pic"/><br />
         <Link href={"/dashboard"}>Dashboard</Link>
         <Link href={"/orders"}>Orders</Link>
         <Link href={"/addproduct"}>Add Product</Link>
--- shopkaro/shopkaro/src/dashboard/pages/dashboard.jsx
+++ shopkaro/shopkaro/src/dashboard/pages/dashboard.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Side from '@/components/Side'
 import styles from '@/styles/Home.module.css'
-const Dashboard = () => {
+import axios from 'axios'
+import { useRouter } from 'next/router'
+const Dashboard = ({orders,product,money,userD}) => {
+const router = useRouter();
+    let totalOrders = orders.length || 0;
+    let totalProducts = product.length || 0;
+    let totalUser = userD.length || 0;
+let sum =0
+money.map((el)=>{
+    sum=el.price;
+})
 
+let totalEarn = "$"+sum;
+const HandleRun = (url)=>{
+router.push(`${url}`)
+}
     return (
         <div className={styles.main}>
     <Side />
@@ -11,13 +25,34 @@ const Dashboard = () => {
     </div>
 
     <div className={styles.grd}>
-        <div><div style={{backgroundColor:"rgba(102, 0, 255, 0.78)"}}>10</div><h3>Total Orders</h3></div>
-        <div><div style={{backgroundColor:"rgba(102, 0, 255, 0.78)"}}>10</div><h3>Total Earning</h3></div>
-        <div><div style={{backgroundColor:"rgba(102, 0, 255, 0.78)"}}>10</div><h3>Total Products</h3></div>
-        <div><div style={{backgroundColor:"rgba(102, 0, 255, 0.78)"}}>10</div><h3>Total Users</h3></div>
+        <div><div onClick={()=>{HandleRun("/orders")}} style={{backgroundColor:"rgba(102, 0, 255, 0.78)"}}>{totalOrders}</div><h3>Total Orders</h3></div>
+        <div><div onClick={()=>{HandleRun("/orders")}} style={{backgroundColor:"rgba(102, 0, 255, 0.78)"}}>{totalEarn}</div><h3>Total Earning</h3></div>
+        <div><div onClick={()=>{HandleRun("/products")}} style={{backgroundColor:"rgba(102, 0, 255, 0.78)"}}>{totalProducts}</div><h3>Total Products</h3></div>
+        <div><div onClick={()=>{HandleRun("/users")}} style={{backgroundColor:"rgba(102, 0, 255, 0.78)"}}>{totalUser}</div><h3>Total Users</h3></div>
     </div>
         </div>
       )
     }
 
 export default Dashboard
+
+export async function getServerSideProps(context) {
+    // orders api 
+    let data = await axios.get("http://localhost:8080/orders")
+    let orders = await data.data;
+
+// all products 
+let productOld = await axios.get("http://localhost:8080/products")
+let product = await productOld.data;
+
+// total earnings 
+let earnings = await axios.get("http://localhost:8080/Order_completed")
+let money = await earnings.data;
+
+// total users
+let user = await axios.get("http://localhost:8080/users")
+let userD = await user.data;
+    return {
+      props: {orders,product,money,userD}, // will be passed to the page component as props
+    }
+  }
