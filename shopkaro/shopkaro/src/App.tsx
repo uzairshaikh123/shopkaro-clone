0034a1ecb2df64f90a6c93d034bd9069dd47a108
Surajy001 <soorajy001@gmail.com>
2023-02-24 04:49:16 +0530

    Home

--- shopkaro/shopkaro/src/App.tsx
+++ shopkaro/shopkaro/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 import Signin from './Pages/Signin';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Navbar from './Components/Navbar';
+import Homepage from './Pages/Homepage';
 function App() {
   return (
 
@@ -11,6 +12,7 @@ function App() {
      <GoogleOAuthProvider clientId="369991425272-n1fpb6hpeds2mp83qef46ftqih1ah03g.apps.googleusercontent.com">
               
     <Navbar />
+    <Homepage />
      <Signin />
   </GoogleOAuthProvider>
     </div>
