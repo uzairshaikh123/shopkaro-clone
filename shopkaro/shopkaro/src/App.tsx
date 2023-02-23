7d07f702af7d0d6268d6e26ca66ca935a7090a8d
Surajy001 <soorajy001@gmail.com>
2023-02-23 06:06:16 +0530

    Navbar

--- shopkaro/shopkaro/src/App.tsx
+++ shopkaro/shopkaro/src/App.tsx
@@ -3,13 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Signin from './Pages/Signin';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import Navbar from './Components/Navbar';
 function App() {
   return (
 
     <div className="App">
      <GoogleOAuthProvider clientId="369991425272-n1fpb6hpeds2mp83qef46ftqih1ah03g.apps.googleusercontent.com">
               
-
+    <Navbar />
      <Signin />
   </GoogleOAuthProvider>
     </div>
